3f4212465cc8b911e1bc67a0abf147f9a10ebc47
22yuto <since2018yuto22@gmail.com>
2019-02-07 02:38:57 +0000

    Usercolmun.add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+
+
+
+private
+  	def user_params
+	    params.require(:user).permit(:name, :email, :profile_image, :introduction)
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_125842) do
+ActiveRecord::Schema.define(version: 2019_02_07_023539) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "recipe_id"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2019_02_06_125842) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.text "profile_image"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
